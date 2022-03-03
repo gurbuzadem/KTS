eb32e57d16609390631281174d3df01237869604
adem gürbüz <adem454@hotmail.com>
2022-03-03 17:38:49 +0300

    ana ekran yapıldı

--- KTS/KTS/Form1.Designer.cs
+++ KTS/KTS/Form1.Designer.cs
@@ -29,13 +29,129 @@ namespace KTS
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(66, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 69);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Yeni Fiş";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(66, 139);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 66);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Müşteriler";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(232, 139);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 66);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Ürünler";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(396, 139);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(121, 66);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Merkezler";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(232, 43);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(121, 69);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Taşıyıcı Firmalar";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(396, 43);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(121, 69);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Kullanıcılar";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(396, 237);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(121, 66);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "Ayarlar";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(232, 237);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(121, 66);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Kontrol Listesi";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(66, 237);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(121, 66);
+            this.button9.TabIndex = 6;
+            this.button9.Text = "Raporlar";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(568, 366);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Ana Form";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
     }
 }
 
